ab590b06faedd300a718b80cedb137273d4bd53c
Acer <strike1985@gmail.com>
2016-07-01 19:22:34 +0300

    calendar

---
+++ scripts/calendar/Calendar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+export default class Calendar extends React.Component {
+    constructor() {
+
+    }
+
+    getInitialState() {
+        return {
+
+        }
+    }
+
+    render() {
+    }
+}
